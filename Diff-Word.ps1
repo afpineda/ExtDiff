1f135c4a91d533161dae5dbebdc41908756000cf
afpineda <74291754+afpineda@users.noreply.github.com>
2021-07-15 16:53:24 +0200

    Code cleaning on Diff-Word.ps1
    Non-mandatory parameters caused resolve function to fail if not provided. try-catch block now covers everything that may fail. Now, MS-Word's windows are kept invisible until needed. This prevents the user from tampering with them while running.

--- Diff-Word.ps1
+++ Diff-Word.ps1
@@ -1,31 +1,35 @@
 param(
+    [Parameter(Mandatory=$true)]
     [string] $BaseFileName,
+    [Parameter(Mandatory=$true)]
     [string] $ChangedFileName
 )
 
 $ErrorActionPreference = 'Stop'
 
-function resolve($relativePath) {
-    (Resolve-Path $relativePath).Path
-}
-
-$BaseFileName = resolve $BaseFileName
-$ChangedFileName = resolve $ChangedFileName
-
-# Remove the readonly attribute because Word is unable to compare readonly
-# files:
-$baseFile = Get-ChildItem $BaseFileName
-if ($baseFile.IsReadOnly) {
-    $baseFile.IsReadOnly = $false
-}
-
 # Constants
 $wdDoNotSaveChanges = 0
 $wdCompareTargetNew = 2
 
+# Auxiliary function
+function resolve($relativePath) {
+    (Resolve-Path $relativePath).Path
+}
+
+# Script body
 try {
+    $BaseFileName = resolve $BaseFileName
+    $ChangedFileName = resolve $ChangedFileName
+
+    # Remove the readonly attribute because Word is unable to compare readonly
+    # files:
+    $baseFile = Get-ChildItem $BaseFileName
+    if ($baseFile.IsReadOnly) {
+        $baseFile.IsReadOnly = $false
+    }
+
     $word = New-Object -ComObject Word.Application
-    $word.Visible = $true
+    $word.Visible = $false
     $document = $word.Documents.Open($BaseFileName, $false, $false)
     $document.Compare($ChangedFileName, [ref]"Comparison", [ref]$wdCompareTargetNew, [ref]$true, [ref]$true)
 
@@ -33,6 +37,7 @@ try {
 
     # Now close the document so only compare results window persists:
     $document.Close([ref]$wdDoNotSaveChanges)
+    $word.Visible = $true
 } catch {
     Add-Type -AssemblyName System.Windows.Forms
     [System.Windows.Forms.MessageBox]::Show($_.Exception)
