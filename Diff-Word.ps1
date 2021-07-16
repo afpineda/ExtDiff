c562cdbe9e3acdbb7cc9455938b8c26300b61791
afpineda <74291754+afpineda@users.noreply.github.com>
2021-07-16 18:52:44 +0200

    Improved implementation

--- Diff-Word.ps1
+++ Diff-Word.ps1
@@ -1,18 +1,23 @@
 <####################################################################
 
     .SYNOPSIS
-        Run Ms-Word as a diff tool
+        Run Ms-Word as a diff tool.
+        Useful for version control.
+
+    .PARAMETER $localFileName
+        Path to first file
+
+    .PARAMETER $remoteFileName
+        Path to second file
 
     .NOTES
-        MS-Word must be installe
+        MS-Word must be installed.
 
 ####################################################################>
 
 param(
-    [ValidateNotNullOrEmpty()]
-    [Parameter(Mandatory=$true)]
-    [string] $BaseFileName,
-    [string] $ChangedFileName
+    [string] $localFileName,
+    [string] $remoteFileName
 )
 
 $ErrorActionPreference = 'Stop'
@@ -20,13 +25,40 @@ $ErrorActionPreference = 'Stop'
 # Constants
 $wdDoNotSaveChanges = 0
 $wdCompareTargetNew = 2
+$wdOpenFormatAuto = 0
+$wdLeftToRight = 0
 
-# Auxiliary function
-function resolve($relativePath) {
-    (Resolve-Path $relativePath).Path
+<####################################################################
+
+    .SYNOPSIS
+        Auxiliary function in order to open MS-Word documents
+
+    .PARAMETER $word
+        Instance of Word.Application
+    
+    .PARAMETER $relativePath
+        Path to file. If $null, an empty
+    
+    .OUTPUTS
+        Document object
+
+####################################################################>
+function open-word-doc($word, $relativePath) {
+    if ($relativePath) {
+        $absPath = (Resolve-Path $relativePath).Path
+        $file = Get-ChildItem $absPath
+        $file.IsReadOnly = $false
+        #$word.Documents.OpenNoRepairDialog($absPath, $false, $true)
+        $word.Documents.OpenNoRepairDialog($absPath, $false, $true, $false,'','',$true,'','',$wdOpenFormatAuto,((Get-WinSystemLocale).LCID), $false,$true,$wdLeftToRight,$true)
+    } else {
+        $word.Documents.Add()
+    }
 }
 
-# Script body
+<####################################################################
+    Script Body
+####################################################################>
+
 try {
     # if MS Word is not installed, fail as soon as possible
     $word = New-Object -ComObject Word.Application
@@ -34,32 +66,17 @@ try {
     # Keep MS-word invisible to prevent user from tampering while running this script
     $word.Visible = $false
 
-    $BaseFileName = resolve $BaseFileName
-    # Remove the readonly attribute because Word is unable to compare readonly
-    # files:
-    $baseFile = Get-ChildItem $BaseFileName
-    if ($baseFile.IsReadOnly) {
-        $baseFile.IsReadOnly = $false
-    }
-    # Open first document
-    $docBase = $word.Documents.Open($BaseFileName, $false, $false)
-
-    # Open second document
-    # NOTE: if not given, use an empty document
-    if ($ChangedFileName) {
-        $ChangedFileName = resolve $ChangedFileName
-        $docChanged = $word.Documents.Open($ChangedFileName, $false, $false)
-    } else {
-        $docChanged = $word.Documents.Add()
-    }
+    # Open documents
+    $docLocal = open-word-doc $word $localFileName
+    $docRemote = open-word-doc $word $remoteFileName
 
     # Open comparison window
-    $docComparison = $word.Application.CompareDocuments($docChanged,$docBase,$wdCompareTargetNew,1)
+    $docComparison = $word.Application.CompareDocuments($docLocal,$docRemote,$wdCompareTargetNew,1)
     $docComparison.Saved  = $true
     
     #close single documents
-    $docBase.Close([ref]$wdDoNotSaveChanges)
-    $docChanged.Close([ref]$wdDoNotSaveChanges)
+    $docLocal.Close([ref]$wdDoNotSaveChanges)
+    $docRemote.Close([ref]$wdDoNotSaveChanges)
 
     # show
     $word.Visible = $true
@@ -69,8 +86,10 @@ try {
         # avoid hidden MS-word instances after failing
         $word.Quit([ref]$false)
     }
+    $line = $_.InvocationInfo.ScriptLineNumber
+
     # show exception text in a message box
     Add-Type -AssemblyName System.Windows.Forms
-    [System.Windows.Forms.MessageBox]::Show($_.Exception,"FAILURE")
+    [System.Windows.Forms.MessageBox]::Show($_.Exception,"FAILURE at line $line")
     exit 1
 }
--- Merge-Word.ps1
+++ Merge-Word.ps1
@@ -1,10 +1,39 @@
-﻿#.\Merge-Word.ps1 "C:\Temp\PruebasGIT\Test1\test.docx" "C:\Temp\PruebasGIT\Test2\test.docx"
-# $d1 = $word.Documents.Open("D:\Documentos\dev\ExtDiff\testCases\d1.docx")
-# $d2 = $word.Documents.Open("D:\Documentos\dev\ExtDiff\testCases\d2.docx")
+﻿<####################################################################
+
+    .SYNOPSIS
+        Run Ms-Word as a merge tool.
+        Useful for version control.
+
+    .PARAMETER $localFileName
+        Path to original file
+
+    .PARAMETER $remoteFileName
+        Path to changed file
+
+    .PARAMETER $mergedFileName
+        Path to merged file
+
+    .PARAMTER $Force
+        If $true, file is merged with no user interaction.
+        Since change tracking is enabled in merge mode, changes
+        may be discarded or accepted later
+  
+    .NOTES
+        MS-Word must be installed.
+        $LASTEXITCODE is 0 if-and-only-if merged file is NOT SAVED by
+        the user.
+
+    .EXAMPLE
+        .\Merge-Word.ps1 "D:\Documentos\dev\ExtDiff\testCases\d1.docx" "D:\Documentos\dev\ExtDiff\testCases\d2.docx" "E:\temp\PruebasGIT\tmp.docx"
+
+####################################################################>
+
+
 param(
     [string] $localFileName,
     [string] $remoteFileName,
-    [string] $destinationFileName
+    [string] $mergedFileName,
+    [switch] $Force
 )
 
 $ErrorActionPreference = 'Stop'
@@ -13,13 +42,29 @@ $ErrorActionPreference = 'Stop'
 $wdDoNotSaveChanges = 0
 $wdCompareTargetNew = 2
 
-# Auxiliary function
+<####################################################################
+
+    .SYNOPSIS
+        Auxiliary function in order to open MS-Word documents
+
+    .PARAMETER $word
+        Instance of Word.Application
+    
+    .PARAMETER $relativePath
+        Path to file. If $null, an empty
+    
+    .OUTPUTS
+        Document object
+
+####################################################################>
+
 function open-word-doc($word, $relativePath) {
     if ($relativePath) {
         $absPath = (Resolve-Path $relativePath).Path
         $file = Get-ChildItem $absPath
         $file.IsReadOnly = $false
-        $word.Documents.Open($absPath, $false, $false)
+        $locale = (Get-WinSystemLocale).LCID
+        $word.Documents.OpenNoRepairDialog($absPath, $false, $true, $false,'','',$true,'','',$wdOpenFormatAuto, $locale, $false,$true,$wdLeftToRight,$true)
     } else {
         $word.Documents.Add()
     }
@@ -40,30 +85,39 @@ try {
 
     # Open merge window
     $docComparison = $word.Application.MergeDocuments($docLocal,$docRemote,$wdCompareTargetNew)
-    if ($destinationFileName) {
-        $docComparison.SaveAs($destinationFileName)
-        $startTimestamp = (Get-ChildItem $destinationFileName).LastWriteTime
+    if ($mergedFileName) {
+        $docComparison.SaveAs($mergedFileName)
+        $startTimestamp = (Get-ChildItem $mergedFileName).LastWriteTime
     }
 
     #close single documents
     $docLocal.Close([ref]$wdDoNotSaveChanges)
     $docRemote.Close([ref]$wdDoNotSaveChanges)
 
-    # show
-    $word.Visible = $true
+    if ($Force -eq $true)
+    {
+        # NON-INTERACTIVE MODE
+        $word.Quit([ref]$false)
+        exit 0
 
-    # Wait for MS-word to close
-    do {
-        sleep(1)
-    } while(!!$word.Application) 
+    } else {
 
-    # test if destination file changed and give exit code
-    if ($destinationFileName) {
-        $currentTimestamp = (Get-ChildItem $destinationFileName).LastWriteTime
-        if ($currentTimestamp -eq $startTimestamp) {
-            exit 128
-        } else {
-            exit 0
+        # INTERACTIVE MODE
+        $word.Visible = $true
+    
+        # Wait for MS-word to close
+        do {
+            sleep(1)
+        } while(!!$word.Application) 
+    
+        # test if destination file changed and give exit code
+        if ($mergedFileName) {
+            $currentTimestamp = (Get-ChildItem $mergedFileName).LastWriteTime
+            if ($currentTimestamp -eq $startTimestamp) {
+                exit 128
+            } else {
+                exit 0
+            }
         }
     }
     exit 0
@@ -72,8 +126,11 @@ try {
         # avoid hidden MS-word instances after failing
         $word.Quit([ref]$false)
     }
+
+    $line = $_.InvocationInfo.ScriptLineNumber
+
     # show exception text in a message box
     Add-Type -AssemblyName System.Windows.Forms
-    [System.Windows.Forms.MessageBox]::Show($_.Exception,"FAILURE")
+    [System.Windows.Forms.MessageBox]::Show($_.Exception,"FAILURE at line $line")
     exit 1
 }
